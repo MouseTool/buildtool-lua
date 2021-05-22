b47f919aa54384cd49af6162c87be684f3b52aa6
Crollie Rollz <crollierollz@gmail.com>
2021-05-23 00:01:12 +0800

    uh wrong ref

--- src/translations/bt-init.lua
+++ src/translations/bt-init.lua
@@ -2,11 +2,11 @@ local localis = require("localisation.localis_manager")
 
 localis.setFallbackLang("en")
 
-localis.mapLangs({
-    {"zh", "cn"},
-})
-
 local translations_tbl = require("translations.translations-gen")
 for lang, data in next, translations_tbl do
     localis.addLanguageData(lang, data)
 end
+
+localis.mapLangs({
+    {"zh", "cn"},
+})
